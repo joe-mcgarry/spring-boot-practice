47ce54a19e140eada67cc5a87a576cc9de7a14f1
Joe McGarry <joewmcgarry@gmail.com>
2022-09-08 14:52:45 +0100

    Adds MessageRepository interface for finding messages in database

--- src/main/kotlin/spring/SpringPracticeApplication.kt
+++ src/main/kotlin/spring/SpringPracticeApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
 
 @SpringBootApplication
 class SpringPracticeApplication
@@ -26,4 +28,7 @@ class MessageResource {
 @Table("messages")
 data class Message(@Id val id: String?, val text: String)
 
-
+interface MessageRepository : CrudRepository<Message, String> {
+	@Query("select * from messages")
+	fun findMessages(): List<Message>
+}
