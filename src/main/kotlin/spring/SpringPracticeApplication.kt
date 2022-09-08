efb5f4b175173182914289757ec17c60bda89bde
Joe McGarry <joewmcgarry@gmail.com>
2022-09-08 14:58:34 +0100

    Adds MessageService class for saving messages to db

--- src/main/kotlin/spring/SpringPracticeApplication.kt
+++ src/main/kotlin/spring/SpringPracticeApplication.kt
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 class SpringPracticeApplication
@@ -31,4 +32,13 @@ data class Message(@Id val id: String?, val text: String)
 interface MessageRepository : CrudRepository<Message, String> {
 	@Query("select * from messages")
 	fun findMessages(): List<Message>
+}
+
+@Service
+class MessageService(val db: MessageRepository) {
+	fun findMessages(): List<Message> = db.findMessages()
+
+	fun post(message: Message) {
+		db.save(message)
+	}
 }
